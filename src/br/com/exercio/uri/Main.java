ebb2d0cd9c142b7705f41a0b4738d151d32c830e
anacosantos <anacarolinaosantos85@gmail.com>
2020-01-11 10:10:47 +0200

    exercicio uri

---
+++ src/br/com/exercio/uri/Main.java
@@ -0,0 +1,42 @@
+package br.com.exercio.uri;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+//		int A, B, X;
+//		
+//		A = sc.nextInt();
+//		B = sc.nextInt();
+//		X = A + B; 
+//		System.out.println("X = " + X);
+		
+//		int A, B, X;
+//	
+//		A = sc.nextInt();
+//		B = sc.nextInt();
+//		X = A + B; 
+//	    System.out.println("SOMA = " + X);
+		
+//	    double A;
+//	    float B, X;
+//	    
+//	    A = sc.nextDouble();
+//	    B = 3.14159f; 
+//	    X = (float) (Math.pow(A, 2) * B);
+//	   
+//	    System.out.printf("A=" + "%.4f%n",X);
+//	    
+	    
+		
+		
+       sc.close();
+		
+	}
+
+}
