b7c5b8ae9b7fbfc6d73bd353347d8f412d5cb23f
Ana Carolina <anacarolinaosantos85@gmail.com>
2020-01-19 21:30:27 +0200

    while exercicios

---
+++ src/br/com/exercio/uri/Test.java
@@ -0,0 +1,31 @@
+package br.com.exercio.uri;
+
+import java.util.Scanner;
+
+public class Test {
+
+	//usuario vai digitar um numero uma unica vez, o programa deve ler esse numero maior q 5 e imprimir o numero anterior a esse até chegar ao numero 5. 
+	//se chegar ao numero cinco o programa deve imprimir "Voce chegou ao cinco" e nao imprimir mais nenhum numero e sair.
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 0;
+		x = sc.nextInt();
+		
+		while (x != 5) {
+			
+			x = x - 1;
+			
+			System.out.println(x);
+			
+			if (x == 5) {
+				System.out.println("Voce chegou ao cinco");
+			}
+		}
+			
+		sc.close();
+	}
+	
+}
