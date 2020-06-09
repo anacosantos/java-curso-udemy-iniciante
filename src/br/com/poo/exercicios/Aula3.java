3071c57efd603e5c8841675f9b33f33bf6f7919b
Ana Carolina <anacarolinaosantos85@gmail.com>
2020-06-09 18:35:48 +0300

    POO

---
+++ src/br/com/poo/exercicios/Aula3.java
@@ -0,0 +1,21 @@
+package br.com.poo.exercicios;
+
+public class Aula3 {
+
+public static void main(String[] args) {
+	
+	CanetaAula3 c1 = new CanetaAula3();
+	c1.modelo = "BIC cristal";
+	c1.cor = "Azul";
+	//c1.ponta = 0.5f;
+	c1.carga = 80;
+	c1.tampar();
+	c1.status();
+	c1.rabiscar();
+	
+	
+}
+	
+	
+}
+
