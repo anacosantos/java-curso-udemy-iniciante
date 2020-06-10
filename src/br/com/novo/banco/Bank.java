173142ef28dc29e232806799f4fa8f3307a48f64
Ana Carolina <anacarolinaosantos85@gmail.com>
2020-06-10 19:23:59 +0300

    Metodologia Get e Set

---
+++ src/br/com/novo/banco/Bank.java
@@ -0,0 +1,9 @@
+package br.com.novo.banco;
+
+public class Bank {
+	 public static void main(String[] args) {
+		
+		 ContaBancoNovo b = new ContaBancoNovo();
+	}
+
+}
---
+++ src/br/com/ooaulas/Aula04.java
@@ -0,0 +1,18 @@
+package br.com.ooaulas;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		//Caneta c1 = new Caneta("ZIC","Amarela",0.4f);
+		//c1.setModelo("Bic");
+		//c1.setPonta(0.5f);
+		//c1.status();
+		// ou o modelo abaixo.
+		//System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+		Caneta c2 = new Caneta("TIC","Verde",0.6f);
+		c2.status();
+		
+
+	}
+
+}
