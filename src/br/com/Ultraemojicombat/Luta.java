5782231043d806aac5fb28b45d1e3b93f2bfdff8
Ana Carolina <anacarolinaosantos85@gmail.com>
2020-06-17 22:15:02 +0300

    Orientação a objeto - extends

---
+++ src/br/com/Ultraemojicombat/Luta.java
@@ -0,0 +1,16 @@
+package br.com.Ultraemojicombat;
+
+public class Luta {
+	//atributos
+	//private desafiado;
+	//private desfiante;
+	private int rounds;
+	private boolean aprovada;
+	
+	public void marcarLuta() {
+		
+	}
+	public void lutar() {
+		
+	}
+}
